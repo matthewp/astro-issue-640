26dd595da5b520ed6a36e734de650ccf0610a9a3
Matthew Phillips <matthew@skypack.dev>
2021-09-28 12:01:48 -0400

    Example working

---
+++ src/components/Button.svelte
@@ -0,0 +1,17 @@
+<script>
+  function toggle() {
+    const html = window.document.head.parentElement
+    const dark = html.classList.toggle('dark')
+    const theme = dark ? 'dark' : 'light'
+    localStorage.setItem('theme', theme)
+  }
+
+  if (!import.meta.env.SSR) {
+    const theme = localStorage.getItem('theme')
+    if (theme === 'dark') toggle()
+  }
+</script>
+
+<button id="theme-switch" aria-label="Trocar tema" on:click={toggle}>
+  Click me
+</button>
---
+++ src/components/SvelteCounter.svelte
@@ -0,0 +1,12 @@
+<script>
+  let count = 0;
+
+  function add() {
+    count++;
+  }
+</script>
+
+
+<button id="theme-switch" aria-label="Trocar tema" on:click={add}>
+  + ({ count })
+</button>
